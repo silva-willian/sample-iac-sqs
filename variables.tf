fdaddd73796a227453866555540eafd9de894d5f
Willian da Silva <willian.silva@fcamara.com.br>
2020-05-29 00:03:11 -0300

    Creation of project variables files

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "env" {
+}
+
+variable "region" {
+}
+
+variable "product" {
+}
+
+variable "project" {
+}
+
+variable "role" {
+}
+
+variable "owner" {
+}
+
+variable "createdBy" {
+}
+
+variable "fifo" {
+}
+
+variable "delay_seconds" {
+}
+
+variable "max_message_size" {
+}
+
+variable "message_retention_seconds" {
+}
+
+variable "receive_wait_time_seconds" {
+}
