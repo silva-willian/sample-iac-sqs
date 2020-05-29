ec705934014000ba666728f04bbcf3838b824af0
Willian da Silva <willian.silva@fcamara.com.br>
2020-05-29 00:07:07 -0300

    Creation of cy / cd destruction artifacts

---
+++ destroy.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+checkSucessful(){
+    if [ $? != 0 ];
+    then
+        echo "Error Execution"
+        exit 1
+    fi
+}
+echo "----------------------------------------"
+echo "Environment variables"
+printenv
+    checkSucessful
+echo "----------------------------------------"
+terraform init \
+    -backend=true \
+    -backend-config="bucket=$REMOTE_STATE_BUCKET-$ENV" \
+    -backend-config="key=$REMOTE_STATE_FILE" \
+    -backend-config="region=$REMOTE_STATE_AWS_REGION"
+    checkSucessful
+echo "----------------------------------------"
+echo "Validating terraform files"
+terraform validate
+    checkSucessful
+echo "----------------------------------------"
+echo "Planning..."
+
+terraform destroy  \
+    -var env="$ENV" \
+    -var region="$AWS_REGION" \
+    -var product="$PRODUCT" \
+    -var createdBy="$CREATED_BY" \
+    -var owner="$OWNER" \
+    -var project="$PROJECT" \
+    -var role="$ROLE" \
+    -var fifo="$FIFO" \
+    -var delay_seconds="$DELAY_SECONDS" \
+    -var max_message_size="$MAX_MESSAGE_SIZE" \
+    -var message_retention_seconds="$MESSAGE_RETENTION_SECONDS" \
+    -var receive_wait_time_seconds="$RECEIVE_WAIT_TIME_SECONDS" \
+    -auto-approve
+    checkSucessful
+echo "----------------------------------------"
